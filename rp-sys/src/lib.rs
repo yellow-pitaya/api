d1668e1becd39b28f6ce82977b98e337a887b5f1
Sanpi <sanpi@homecomputing.fr>
2017-05-05 20:04:49 +0200

    Impl From<String> for rp_dpin_t

--- rp-sys/src/lib.rs
+++ rp-sys/src/lib.rs
@@ -22,3 +22,35 @@ impl ::std::default::Default for rp_calib_params_t {
         }
     }
 }
+
+impl ::std::convert::From<String> for rp_dpin_t {
+    fn from(s: String) -> Self {
+        match s.as_str() {
+            "LED0" => rp_dpin_t::RP_LED0,
+            "LED1" => rp_dpin_t::RP_LED1,
+            "LED2" => rp_dpin_t::RP_LED2,
+            "LED3" => rp_dpin_t::RP_LED3,
+            "LED4" => rp_dpin_t::RP_LED4,
+            "LED5" => rp_dpin_t::RP_LED5,
+            "LED6" => rp_dpin_t::RP_LED6,
+            "LED7" => rp_dpin_t::RP_LED7,
+            "DIO0_P" => rp_dpin_t::RP_DIO0_P,
+            "DIO1_P" => rp_dpin_t::RP_DIO1_P,
+            "DIO2_P" => rp_dpin_t::RP_DIO2_P,
+            "DIO3_P" => rp_dpin_t::RP_DIO3_P,
+            "DIO4_P" => rp_dpin_t::RP_DIO4_P,
+            "DIO5_P" => rp_dpin_t::RP_DIO5_P,
+            "DIO6_P" => rp_dpin_t::RP_DIO6_P,
+            "DIO7_P" => rp_dpin_t::RP_DIO7_P,
+            "DIO0_N" => rp_dpin_t::RP_DIO0_N,
+            "DIO1_N" => rp_dpin_t::RP_DIO1_N,
+            "DIO2_N" => rp_dpin_t::RP_DIO2_N,
+            "DIO3_N" => rp_dpin_t::RP_DIO3_N,
+            "DIO4_N" => rp_dpin_t::RP_DIO4_N,
+            "DIO5_N" => rp_dpin_t::RP_DIO5_N,
+            "DIO6_N" => rp_dpin_t::RP_DIO6_N,
+            "DIO7_N" => rp_dpin_t::RP_DIO7_N,
+            _ => unimplemented!(),
+        }
+    }
+}
