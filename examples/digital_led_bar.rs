a2ac2bac45082e2bc66988bba4cc996f387c8df2
Sanpi <sanpi@homecomputing.fr>
2017-05-17 11:48:31 +0200

    Add digital_led_bar example

---
+++ examples/digital_led_bar.rs
@@ -0,0 +1,40 @@
+extern crate redpitaya;
+
+use redpitaya::pin::State;
+use redpitaya::pin::digital::Pin;
+
+fn main() {
+    let leds = [
+        Pin::RP_LED0,
+        Pin::RP_LED1,
+        Pin::RP_LED2,
+        Pin::RP_LED3,
+        Pin::RP_LED4,
+        Pin::RP_LED5,
+        Pin::RP_LED6,
+        Pin::RP_LED7,
+    ];
+
+    let percent = match std::env::args().nth(1) {
+        Some(arg) => arg.parse().unwrap(),
+        None => 50.0,
+    };
+
+    redpitaya::init()
+        .expect("Red Pitaya API init failed!");
+
+
+    for x in 0..leds.len() {
+        let state = if percent > (x as f32 * 100.0 / 8.0) {
+            State::RP_HIGH
+        } else {
+            State::RP_LOW
+        };
+
+        redpitaya::pin::digital::set_state(leds[x], state)
+            .unwrap();
+    }
+
+    redpitaya::release()
+        .unwrap();
+}
