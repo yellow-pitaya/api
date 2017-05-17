5ac05f74f3d49e06fa6003c3d44249fa8b477b06
Sanpi <sanpi@homecomputing.fr>
2017-05-17 12:36:39 +0200

    Add digital_push_button example

---
+++ examples/digital_push_button.rs
@@ -0,0 +1,50 @@
+extern crate redpitaya;
+
+use redpitaya::pin::Direction;
+use redpitaya::pin::digital::Pin;
+
+fn main() {
+    let pins = [
+        Pin::RP_DIO0_N,
+        Pin::RP_DIO1_N,
+        Pin::RP_DIO2_N,
+        Pin::RP_DIO3_N,
+        Pin::RP_DIO4_N,
+        Pin::RP_DIO5_N,
+        Pin::RP_DIO6_N,
+        Pin::RP_DIO7_N,
+    ];
+
+    let leds = [
+        Pin::RP_LED0,
+        Pin::RP_LED1,
+        Pin::RP_LED2,
+        Pin::RP_LED3,
+        Pin::RP_LED4,
+        Pin::RP_LED5,
+        Pin::RP_LED6,
+        Pin::RP_LED7,
+    ];
+
+    redpitaya::init()
+        .expect("Red Pitaya API init failed!");
+
+    for x in 0..pins.len() {
+        redpitaya::pin::digital::set_direction(pins[x], Direction::RP_IN)
+            .unwrap();
+    }
+
+    loop {
+        for x in 0..pins.len() {
+            let state = redpitaya::pin::digital::get_state(pins[x])
+                .unwrap();
+
+            redpitaya::pin::digital::set_state(leds[x], state)
+                .unwrap();
+        }
+    }
+
+    redpitaya::release()
+        .unwrap();
+}
+
