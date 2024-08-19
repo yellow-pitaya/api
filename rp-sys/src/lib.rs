99d1911771ec9c316fe95282d94ca5781f35ab43
Sanpi <sanpi@homecomputing.fr>
2024-08-19 14:09:51 +0200

    [rp-sys] Updates bindgen to 0.70

--- rp-sys/src/lib.rs
+++ rp-sys/src/lib.rs
@@ -1,9 +1,7 @@
-#![allow(unused_variables)]
-#![allow(non_snake_case)]
 #![allow(non_camel_case_types)]
 #![allow(non_upper_case_globals)]
-#![allow(clippy::unreadable_literal)]
-#![allow(clippy::missing_safety_doc)]
+#![allow(clippy::identity_op)]
+#![allow(clippy::unnecessary_operation)]
 #![warn(warnings)]
 
 #[cfg(not(feature = "mock"))]
